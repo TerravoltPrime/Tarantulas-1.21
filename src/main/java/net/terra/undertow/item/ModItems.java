1a84faa76eea5c28e6e6a604ccdee2a0ac073ae0
terra <terravolt.mc@gmail.com>
2024-10-08 15:34:57 -0400

    item and block registries

---
+++ src/main/java/net/terra/undertow/item/ModItems.java
@@ -0,0 +1,8 @@
+package net.terra.undertow.item;
+
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(theUndertow.MOD_ID);
+
+}
